32278d52290772c6b20c4ce831ebb3ecae732b84
JekyllAndHyde8999 <sravanvinakota@gmail.com>
2022-01-10 21:25:49 -0500

    Fixed URL checking

--- ShortenerExtension/scripts/background.js
+++ ShortenerExtension/scripts/background.js
@@ -32,12 +32,7 @@ function getUrl(tab) {
 }
 
 function shortenUrl(url) {
-    regex = new RegExp('^(https?:\\/\\/)?' + // protocol
-        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-        '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-        '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-        '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+    regex = new RegExp('^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$');
     if (!!regex.test(url)) {
         response = fetch("https://tinyurl.com/api-create.php?url=".concat(encodeURI(url))).then(res => res.text());
         return response;
